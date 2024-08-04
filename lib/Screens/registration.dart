fa954e4c04a42314983c1760dee54a419b8952ac
adeenazara2002 <adinazara2001@gmail.com>
2024-08-04 14:12:53 +0500

    Registration

--- lib/Screens/registration.dart
+++ lib/Screens/registration.dart
@@ -34,19 +34,24 @@ class _SplashScreenState extends State<SplashScreen> {
                       ),
                       child: Column(
                         children: [
+                          SizedBox(
+                            width: 20,
+                          ),
                           Column(
                             children: [
                               Row(
                                 children: [
                                   Column(
                                     children: [
+                                      // Padding(padding: EdgeInsets.only(left: 27)),
                                       SizedBox(
-                                          height:
-                                              200), // Adjust this value as needed
+                                        width: 20,
+                                      ),
+
+                                      SizedBox(height: 200),
+                                      // Padding(padding: EdgeInsets.only(left: 27)),
                                       Image.asset('assets/images/logo.png'),
-                                      SizedBox(
-                                          height:
-                                              20), // Add some spacing between the image and the text
+                                      SizedBox(height: 20),
                                       Text(
                                         'Full Name',
                                         style: TextStyle(
@@ -136,7 +141,7 @@ class _SplashScreenState extends State<SplashScreen> {
                                 ],
                               ),
 
-                              // email text column
+                              // email text column end
 
                               // email field column
 
@@ -191,6 +196,167 @@ class _SplashScreenState extends State<SplashScreen> {
                                   ),
                                 ],
                               ),
+
+                              // email column ended
+
+// pass text column
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                          padding: EdgeInsets.only(left: 30)),
+                                      Text(
+                                        'Password',
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+
+                              // pass text column end
+
+                              // password field column
+
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                          padding: EdgeInsets.only(
+                                              left: 27, top: 30)),
+
+                                      // container 1
+                                      SizedBox(
+                                        height: 40,
+                                      ),
+
+                                      Container(
+                                        // padding: EdgeInsets.only(top: 20),
+                                        height: 60,
+                                        width: 350,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          border: Border.all(
+                                            color: Color.fromRGBO(
+                                                217, 217, 217, 0.96),
+                                          ),
+                                        ),
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            hintText: 'Enter your password',
+                                            hintStyle: TextStyle(
+                                                color:
+                                                    Color.fromRGBO(0, 0, 0, 1),
+                                                fontFamily: 'Poppins-Regular',
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w400),
+                                            border: InputBorder.none,
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                              vertical: 20.0,
+                                              horizontal: 10.0,
+                                            ),
+                                          ),
+                                          style: TextStyle(
+                                            color: Color.fromRGBO(
+                                                143, 143, 158, 1),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+
+                              // password field column ended
+
+                              // button
+
+                              // column 8
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                          padding: EdgeInsets.only(left: 27)),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              Color.fromRGBO(170, 20, 240, 1),
+                                          foregroundColor:
+                                              Color.fromRGBO(255, 255, 255, 1),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 100.0,
+                                              vertical: 20.0),
+                                          child: Text(
+                                            'Create Account',
+                                            style: TextStyle(
+                                                fontSize: 15.0,
+                                                fontFamily: 'Poppins-Regular',
+                                                fontWeight: FontWeight.w400),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              // button column ended
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                          padding: EdgeInsets.only(left: 100)),
+                                      Text(
+                                        'Already have an account?',
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                          fontFamily: 'Poppins-Regular',
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+
+                                      // another text
+                                      Padding(
+                                          padding: EdgeInsets.only(left: 5)),
+                                      Text(
+                                        'Sign In',
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                          fontFamily: 'Poppins-Regular',
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ],
                           ),
                         ],
@@ -198,7 +364,13 @@ class _SplashScreenState extends State<SplashScreen> {
                     ),
                   ],
                 ),
+                // purple column text
+Padding(padding: EdgeInsets.only(top: 20)),
+                Text('Copyright 2024' , 
+                style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1),
+                fontSize: 15, fontWeight: FontWeight.w400),)
               ],
+              
             ),
           ),
         ),
