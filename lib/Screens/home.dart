df4ea585d66fc5fe14353a4180b4137f9e8cc904
adeenazara2002 <adinazara2001@gmail.com>
2024-08-04 16:07:22 +0500

    Home

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -53,10 +54,74 @@ class _HomeState extends State<Home> {
               // ======== HEADER Complete ========
 
               // ======== PURPLE CONTAINER ========
+
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 20)),
+                     Container(
+                  
+                        height: 150,
+                        width: 360,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(170, 20, 240, 1),
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(
+                            color: Color.fromRGBO(170, 20, 240, 1),
+                          ),
+                        ),
+                        child: Column(
+                          // crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(padding: EdgeInsets.only(left: 30, top: 70)),
+                                    Text('Nike Air Max 270', style: 
+                                    TextStyle(fontWeight: FontWeight.w700, color: Color.fromRGBO(255, 255, 255, 1), fontSize: 18),),
+                                    
+                                  ],
+                                ),
+                              
+                                Row(
+                                 
+                                  children: [
+                                    
+                                    Padding(padding: EdgeInsets.only(left: 30,)),
+                                    Text('Men Shoe', style: 
+                                    TextStyle(fontWeight: FontWeight.w600, color: Color.fromRGBO(156, 160, 175, 1), fontSize: 13),),
+                                  ],
+                                ),
+
+                                Row(
+                                 
+                                  children: [
+                                    
+                                    Padding(padding: EdgeInsets.only(left: 30,)),
+                                    Text('290.00', style: 
+                                    TextStyle(fontWeight: FontWeight.w700, color: Color.fromRGBO(255, 255, 255, 1), fontSize: 23),),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                   
+                
+              ),
             ],
           ),
-        )),
+                ]
+        ),
+            ]
+        ),
+            )
+        ),
       ),
     );
+      
+    
   }
 }
